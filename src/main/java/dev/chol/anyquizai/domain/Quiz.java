fcb27b793933f132ad5b1b9d53e7f4f67f4ba399
Chol Nhial <chol.nhial.chol@gmail.com>
2024-02-24 18:55:50 +1000

    create base quiz entity

--- src/main/java/dev/chol/anyquizai/domain/Quiz.java
+++ src/main/java/dev/chol/anyquizai/domain/Quiz.java
@@ -1,4 +1,35 @@
 package dev.chol.anyquizai.domain;
 
+import dev.chol.anyquizai.domain.enumeration.Difficulty;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.ZonedDateTime;
+
+@Entity
+@Table
+@Getter
+@Setter
 public class Quiz {
+
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    private Long id;
+
+    @Column
+    private String uniqueCode;
+
+    @Column
+    private String title;
+
+    @Column
+    private Long totalQuestions;
+
+    @Column
+    @Enumerated(EnumType.STRING)
+    private Difficulty difficulty;
+
+    @Column
+    private ZonedDateTime created;
 }
---
+++ src/main/java/dev/chol/anyquizai/domain/enumeration/Difficulty.java
@@ -0,0 +1,7 @@
+package dev.chol.anyquizai.domain.enumeration;
+
+public enum Difficulty {
+    EASY,
+    MEDIUM,
+    HARD
+}
