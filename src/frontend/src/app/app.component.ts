5c22848727287e269c7cab3865e6cf7e413c70db
Chol Nhial <chol.nhial.chol@gmail.com>
2024-04-28 12:21:13 +1000

    Add flowbite tailwind UI

--- src/frontend/src/app/app.component.ts
+++ src/frontend/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import {Component} from '@angular/core';
 import {RouterOutlet} from '@angular/router';
+import {initFlowbite} from "flowbite";
 
 @Component({
   selector: 'app-root',
@@ -10,4 +11,8 @@ import {RouterOutlet} from '@angular/router';
 })
 export class AppComponent {
   title = 'frontend';
+
+  ngOnInit(): void {
+    initFlowbite();
+  }
 }
--- src/frontend/src/app/app.routes.ts
+++ src/frontend/src/app/app.routes.ts
@@ -1,3 +1,25 @@
 import {Routes} from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/discover',
+    pathMatch: 'full',
+  },
+  {
+    path: 'discover',
+    loadComponent: () => import('./pages/discover/discover.component').then(m => m.DiscoverComponent)
+  },
+  {
+    path: 'create',
+    loadComponent: () => import('./pages/create/create.component').then(m => m.CreateComponent)
+  },
+  {
+    path: 'play/:quizId',
+    loadComponent: () => import('./pages/play/play.component').then( m => m.PlayComponent)
+  },
+  {
+    path: 'finish',
+    loadComponent: () => import('./pages/finish/finish.component').then( m => m.FinishComponent)
+  },
+];
