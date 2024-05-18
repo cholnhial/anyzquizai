193003afd58b9db2725b2b3f3b8f87db227e016e
Chol Nhial <chol.nhial.chol@gmail.com>
2024-05-18 13:28:22 +1000

    allow search with search filters

--- src/frontend/src/app/pages/discover/discover.component.ts
+++ src/frontend/src/app/pages/discover/discover.component.ts
@@ -4,11 +4,12 @@ import {ICategory} from "../../models/category.model";
 import {HttpResponse} from "@angular/common/http";
 import {CommonModule} from "@angular/common";
 import {IQuiz} from "../../models/quiz.model";
+import {FormsModule} from "@angular/forms";
 
 @Component({
   selector: 'app-discover',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './discover.component.html',
   styleUrl: './discover.component.scss'
 })
@@ -16,10 +17,30 @@ export class DiscoverComponent implements OnInit {
 
   categories: ICategory[] = [];
   quizzes: IQuiz[] = [];
+  searchOptions = {
+    page: 0,
+    size: 6,
+    title: '',
+    categoryId: undefined,
+    difficulty: undefined,
+    questions: undefined,
+    sort_created: 'DESC',
+    sort_difficulty: 'ASC'}
+
 
   constructor(private quizService: QuizService) {
   }
 
+  computeSearchParams() {
+    return Object.entries(this.searchOptions)
+      .filter(([key, value]) => !!value) // Filter out falsy values
+      .reduce((acc, [key, value]) => {
+        return {
+          ...acc,
+          [key]: value
+        };
+      }, {});
+  }
 
   ngOnInit(): void {
     this.quizService.getAllCategories().subscribe({
@@ -28,11 +49,20 @@ export class DiscoverComponent implements OnInit {
       }
     });
 
-    this.quizService.search({page: 1}).subscribe({
+    this.loadQuizzes();
+
+  }
+
+  loadQuizzes() {
+    this.quizService.search(this.computeSearchParams()).subscribe({
       next: (resp: HttpResponse<any>) => {
         this.quizzes = resp.body.content || [];
       }
     })
+  }
+
+  onSearch() {
+    this.loadQuizzes();
   }
 
 }
