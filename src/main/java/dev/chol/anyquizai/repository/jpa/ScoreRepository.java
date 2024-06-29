23e0e3d90faab2e3b893c18636021955380187ed
Chol Nhial <chol.nhial.chol@gmail.com>
2024-06-29 18:12:38 +1000

    handle score submission nickname taken exceptions and make it possible to reuse nickname on different quiz

--- src/main/java/dev/chol/anyquizai/repository/jpa/ScoreRepository.java
+++ src/main/java/dev/chol/anyquizai/repository/jpa/ScoreRepository.java
@@ -5,8 +5,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface ScoreRepository extends JpaRepository<Score, Long> {
     List<Score> findAllByQuizIdOrderByScoreDesc(Long id);
+    Optional<Score> findOneByNickName(String nickName);
 }
--- src/main/java/dev/chol/anyquizai/service/ScoreService.java
+++ src/main/java/dev/chol/anyquizai/service/ScoreService.java
@@ -3,12 +3,14 @@ package dev.chol.anyquizai.service;
 import dev.chol.anyquizai.domain.jpa.Quiz;
 import dev.chol.anyquizai.domain.jpa.Score;
 import dev.chol.anyquizai.dto.QuizScoreSubmissionRequestDTO;
+import dev.chol.anyquizai.exception.NickNameTakenException;
 import dev.chol.anyquizai.repository.jpa.ScoreRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @Transactional(readOnly = true)
@@ -31,6 +33,9 @@ public class ScoreService {
            Quiz quiz = quizService.getQuizById(scoreDto.quizId());
            Score newQuizScore = scoreDto.toScore(quiz.getTotalQuestions());
            newQuizScore.setQuiz(quiz);
+           if (getOneByNickname(scoreDto.nickname()).isPresent()) {
+               throw new NickNameTakenException(scoreDto.nickname());
+           }
            return scoreRepository.save(newQuizScore);
     }
 
@@ -42,6 +47,10 @@ public class ScoreService {
      */
     public List<Score> getAllByQuizIdOrderedByScore(Long quizId) {
         return scoreRepository.findAllByQuizIdOrderByScoreDesc(quizId);
+    }
+
+   public Optional<Score> getOneByNickname(String nickname) {
+        return scoreRepository.findOneByNickName(nickname);
     }
 
 }
