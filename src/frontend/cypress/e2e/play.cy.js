808ea3533ab0094f6d64d98994cfc5c9e3f2c8ca
Chol Nhial <chol.nhial.chol@gmail.com>
2024-09-08 10:22:03 +1000

    code formating and fixing tests

---
+++ src/frontend/cypress/e2e/play.cy.js
@@ -0,0 +1,19 @@
+describe("Play Page", () => {
+
+  it.skip('should show first question and possible answers', () => {
+
+  })
+
+  it.skip('should show error when I click the wrong answer', () => {
+
+  })
+
+  it.skip('should show feedback when I click on the right answer', () => {
+
+  })
+
+  it.skip('should show score when I switch to score tab', () => {
+
+  })
+
+})
